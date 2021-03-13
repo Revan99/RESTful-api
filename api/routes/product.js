b3327b258973ffdf715a0b69fe7eeb3854e5df60
Revan Sarbast Salih <revansarbast8@gmail.com>
2021-03-13 10:47:39 +0300

    connec to mongodb

--- api/routes/product.js
+++ api/routes/product.js
@@ -1,39 +1,85 @@
 const express = require("express")
 const router = express.Router();
+const mongoose = require("mongoose")
+const Product = require('./../models/product')
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message : 'handling GET request to /products'
-    })
+    Product.find()
+        .exec()
+        .then(docs => {
+            console.log(docs);
+            res.status(200).json(docs)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({err})
+        })
 })
 
 router.post('/', (req, res, next) => {
-    res.status(201).json({
-        message : 'handling POST request to /products'
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+    })
+    product.save()
+        .then((result) => {
+            console.log(result)
+            res.status(201).json({
+            message: 'handling POST request to /products',
+            createdProduct: result
+        })
+                .catch((err) => {
+                    consol.log(err)
+                    res.status(201).json({error: err})
+                })
+        
     })
 })
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId
-    if (id === 'special') {
-        res.status(200).json({
-            message: 'you dscoverd the special ID',
-            id : id
+    console.log(id);
+    Product.findById(id)
+        .exec()
+        .then((result) => {
+            console.log("From database", result)
+            if (result)
+                res.status(200).json(result)
+            else
+                res.status(404).json({message: "No valid entry found for providing ID"})
+                
         })
-    } else {
-        res.status(200).json({
-            message: 'you passed an ID'
+        .catch((err) => {     
+            console.log(err)
+            res.status(500).json({error: err})
         })
-    }
 })
 router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-       message:"updated product"
-   })
+    const id = req.params.productId
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value
+    }
+    Product.findByIdAndUpdate({ _id: id }, { $set: updateOps }).exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json(result)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({err})
+        })
 })
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-       message:"deleted product"
-   })
+    const id = req.params.productId
+    Product.remove({ _id: id })
+        .exec()
+        .then(result => {
+            res.status(200).json(result)
+        })
+        .catch(err => {
+            res.status(500).json({err})
+        })
 })
 module.exports = router
--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-
+const bodyParser = require('body-parser')
 const productRoutes = require('./api/routes/product')
 const ordersRoutes = require('./api/routes/orders')
+const mongoose = require('mongoose')
 
+mongoose.connect('mongodb+srv://Revan99:'+ process.env.MONGO_ATLAS_PW +'@cluster0.78v5t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useMongoClient: true})
 //login resquest
 app.use(morgan("dev"))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Header', 'Origin, X-Request-with, Content-Type, accept, Authorization')
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, DELETE, GET')
+        return res.status(200).json({})
+    }
+    next();
+})
 //Routes which should handel reqests
 app.use('/products', productRoutes)
 app.use('/orders', ordersRoutes)
